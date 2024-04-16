0c1d630607af80a92684a9f22d2b1b97dfeb3416
derammo <817160+derammo@users.noreply.github.com>
2024-04-16 16:30:05 -0400

    timeout with custom message

--- main/PromiseTimeout.ts
+++ main/PromiseTimeout.ts
@@ -1,10 +1,10 @@
 
-export function withTimeout<T>(future: Promise<T>, timeoutMilliseconds: number): Promise<T> {
+export function withTimeout<T>(future: Promise<T>, timeoutMilliseconds: number, customMessage?: string): Promise<T> {
     let timer: NodeJS.Timeout | undefined = undefined;
     return Promise.race([
         future,
         new Promise<never>((_resolve, reject) => {
-            timer = setTimeout(() => reject(`timed out after ${timeoutMilliseconds}ms`), timeoutMilliseconds);
+            timer = setTimeout(() => reject(customMessage ?? `timed out after ${timeoutMilliseconds}ms`), timeoutMilliseconds);
         })
     ]).finally(() => {
         // cancel timer in case we succeeded
